56c4aca3ca4bba316483273610213a8cede8e7ac
AldoGutierrezD <aldogd24@gmail.com>
2026-01-05 10:23:01 -0600

    Se añade el módulo para ver Entradas

---
+++ portal/entradaPDF.php
@@ -0,0 +1,283 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+include("servicios/conexioni.php");
+require_once('servicios/tcpdf/tcpdf.php');
+require('servicios/phpqrcode/qrlib.php');
+setlocale(LC_ALL, 'es_ES');
+mysqli_set_charset($mysqli, 'utf8');
+
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = (int)$_GET['id'];
+}
+
+
+
+//SUCURSAL
+#region
+$qsucursal = "SELECT * FROM ct_sucursales where pk_sucursal = (select fk_sucursal from tr_entradas where pk_entrada=$id)";
+
+if (!$rsucursal = $mysqli->query($qsucursal)) {
+    echo "Lo sentimos, esta aplicación está experimentando problemas1.";
+    exit;
+}
+$empresa = $rsucursal->fetch_assoc();
+$empresa_nombre = $empresa["nombre"];
+$empresa_id = $empresa["pk_sucursal"];
+$empresa_direccion = $empresa["direccion"];
+$empresa_telefono = $empresa["telefono"];
+$empresa_correo = $empresa["correo"];
+#endregion
+
+
+
+//ENCABEZADO
+#region
+$qentrada = "SELECT * FROM tr_entradas WHERE pk_entrada = $id";
+
+if (!$rentrada = $mysqli->query($qentrada)) {
+    echo "Lo sentimos, esta aplicación está experimentando problemas2.";
+    exit;
+}
+
+$entrada = $rentrada->fetch_assoc();
+$entrada_fecha = $entrada["fecha"];
+$entrada_hora = $entrada["hora"];
+$entrada_fk_usuario = $entrada["fk_usuario"];
+$entrada_contrato = $entrada["fk_compra"];
+$entrada_observaciones = $entrada["observaciones"];
+$entrada_total = $entrada["total"];
+#endregion
+
+
+
+//¿ES COMPRA?
+#region
+if ($entrada_contrato != 0) {
+
+    $qcontrato = "SELECT * FROM tr_compras WHERE pk_compra = $entrada_contrato";
+
+    if (!$rcontrato = $mysqli->query($qcontrato)) {
+        echo "Lo sentimos, esta aplicación está experimentando problemas2.";
+        exit;
+    }
+
+    $contrato = $rcontrato->fetch_assoc();
+    $contrato_fecha = $contrato["fecha"];
+    $contrato_proveedor = $contrato["fk_proveedor"];
+    $contrato_usuario = $contrato["fk_usuario"];
+    $contrato_total = $contrato["total"];
+    $contrato_saldo = $contrato["saldo"];
+
+    $anticipo = 0.00;
+
+    if ($contrato_pago != null && $contrato_pago != 0) {
+        $anticipo = $contrato_pago;
+    }
+
+    $qproveedor = "SELECT * FROM ct_proveedores WHERE pk_proveedor=$contrato_proveedor";
+
+    if (!$rproveedor = $mysqli->query($qproveedor)) {
+        echo "Lo sentimos, esta aplicación está experimentando problemas4.";
+        exit;
+    }
+    $proveedor = $rproveedor->fetch_assoc();
+    $proveedor_nombre = $proveedor["nombre"];
+    $proveedor_telefono = $proveedor["telefono"];
+    $proveedor_correo = $proveedor["correo"];
+} else {
+
+    $proveedor_nombre = "NA";
+    $proveedor_telefono = "NA";
+    $proveedor_correo = "NA";
+}
+#endregion
+
+
+
+
+//CALCULAR LA ALTURA DEL TICKET
+#region
+$mysqli->next_result();
+
+if (!$venta_detalle_registros = $mysqli->query("SELECT COUNT(*) as registros FROM tr_entradas_detalle WHERE fk_entrada = $id AND estado = 1")) {
+    echo "Lo sentimos, esta aplicación está experimentando problemas. Error al calcular las dimensiones del ticket";
+    exit;
+}
+
+$rowvdr = $venta_detalle_registros->fetch_assoc();
+$total_registros = $rowvdr['registros'];
+
+$page_height = ($total_registros + 11) * 10 + 170;
+#endregion
+
+
+
+
+//CREAR PDF
+#region
+$pageLayout = array(80, $page_height);
+$pdf = new TCPDF('P', PDF_UNIT, $pageLayout, true, 'UTF-8', false);
+
+$pdf->SetCreator("POSMOVIL");
+$pdf->SetAuthor('POSMOVIL');
+$pdf->SetTitle("ENTRADA #" . $id);
+
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+$pdf->SetMargins(5, 5, 5);
+
+$pdf->SetAutoPageBreak(FALSE, 5);
+
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+if (@file_exists(dirname(__FILE__) . '/lang/eng.php')) {
+    require_once(dirname(__FILE__) . '/lang/eng.php');
+    $pdf->setLanguageArray($l);
+}
+
+$fontname = TCPDF_FONTS::addTTFfont('../ws/tcpdf/fonts/quicksand/Quicksand-Regular.ttf', 'TrueTypeUnicode', '');
+$pdf->SetFont('dejavusans', '', 10);
+$pdf->AddPage();
+#endregion
+
+
+
+
+//HEADER
+#region
+$parrafo_header = <<<HTML
+    <table border="0" cellpadding="1">
+        <tr>
+            <td colspan="2">
+                <h3 style="text-align: center;">ENTRADA #$id</h3>
+                <h5 style="text-align: center; font-weight: light;">$empresa_direccion</h5>
+                <h5 style="text-align: center; font-weight: light;">Tel $empresa_telefono</h5>
+                <h5 style="text-align: center; font-weight: light;">$entrada_fecha $entrada_hora</h5>
+                <h5 style="text-align: center; font-weight: light;"> <span style="font-weight: bold;">PROVEEDOR:</span> $proveedor_nombre ($proveedor_telefono)</h5>
+            </td>
+        </tr>
+    </table>
+    HTML;
+#endregion
+
+
+
+
+//CODIGO QR
+#region
+$url = "https://posmovil.integracontrol.online/portal/entradaPDF.php?id=$id";
+$qr_filename = "entrada_qr.png";
+QRcode::png($url, $qr_filename);
+#endregion
+
+
+
+
+//REGISTROS
+#region
+if ($entrada_contrato != 0) {
+    $qentradasd = "SELECT tr_entradas_detalle.fk_producto as fk_producto,
+        tr_entradas_detalle.fk_producto_nombre as fk_producto_nombre,
+        tr_entradas_detalle.cantidad as cantidad,
+        tr_entradas_detalle.serie,
+        tr_compras_detalle.faltante as faltante,
+        tr_compras_detalle.unitario,
+        ct_productos.clave
+        FROM tr_entradas_detalle, tr_compras_detalle, ct_productos
+        WHERE tr_compras_detalle.fk_producto=tr_entradas_detalle.fk_producto
+        AND tr_entradas_detalle.fk_compra=tr_compras_detalle.fk_compra
+        AND tr_entradas_detalle.fk_entrada=$id
+        AND tr_entradas_detalle.fk_producto = ct_productos.pk_producto";
+} else {
+
+    $qentradasd = "SELECT tr_entradas_detalle.*,
+        ct_productos.clave
+        FROM tr_entradas_detalle, ct_productos
+        WHERE tr_entradas_detalle.fk_entrada = $id
+        AND ct_productos.pk_producto = tr_entradas_detalle.fk_producto";
+}
+
+$mysqli->next_result();
+if (!$rentradasd = $mysqli->query($qentradasd)) {
+    echo "Lo sentimos, esta aplicación está experimentando problemas. Error al obtener el detalle de la compra";
+    exit;
+}
+
+$cantidad_total = 0;
+$tr_table = "";
+while ($row = $rentradasd->fetch_assoc()) {
+
+    $cantidad_total += $row['cantidad'];
+
+    $descripcion = "(" . $row['clave'] . "). " . $row['fk_producto_nombre'] . ". FALTANTE: #" . $row['faltante'];
+
+    $tr_table .= <<<HTML
+        <tr>
+            <td style="width: 15%;">$row[cantidad]</td>
+            <td style="width: 60%;">$descripcion</td>
+            <td style="width: 25%; text-align: right;">$$row[unitario]</td>
+        </tr>
+    HTML;
+}
+
+$contrato_total = number_format($entrada_total, 2);
+
+$content_table = <<<HTML
+    <table border="1" cellpadding="2" style="text-align: center; font-size: 10px;">
+        <thead>
+            <tr style="font-weight: bold;">
+                <th style="width: 15%;">#</th>
+                <th style="width: 60%;">PRODUCTO</th>
+                <th style="width: 25%;">PRECIO</th>
+            </tr>
+        </thead>
+        <tbody>
+            $tr_table
+            <tr>
+                <td colspan="3" style="text-align: left;">Cantidad de productos: $cantidad_total</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="2" style="width: 75%; text-align: right;">TOTAL:</td>
+                <td colspan="1" style="width: 25%; text-align: right;">$$contrato_total</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;">OBSERVACIONES: $entrada_observaciones</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: center;">
+                    <img src="$qr_filename" width="90" height="90" />
+                </td>
+            </tr>
+        </tfoot>
+    </table>
+HTML;
+#endregion
+
+
+
+
+//IMPRIMIR
+#region
+$pdf->Image('@' . file_get_contents('servicios/logo.png'), 30, 5, 0, 24);
+$pdf->writeHTMLCell(70, '', 5, 25, $parrafo_header);
+$pdf->writeHTMLCell(70, '', 5, 70, $content_table);
+#endregion
+
+
+
+
+//DETALLES FINALES
+#region
+// reset pointer to the last page
+$pdf->lastPage();
+
+// Obtener el ancho y alto de la página, teniendo en cuenta los márgenes
+$anchoPagina = $pdf->getPageWidth() - 8 - 8;
+$altoPagina = $pdf->getPageHeight() - 8 - 8;
+
+//Close and output PDF document
+$fileName = "Entrada_" . $id . ".pdf";
+$pdf->Output($fileName, 'I');
+#endregion
---
+++ portal/verEntradas.php
@@ -0,0 +1,207 @@
+<?php
+header('Cache-control: private');
+include("servicios/conexioni.php");
+@session_start();
+
+if (!isset($_SESSION['usuario'])) {
+    header('Location: ../index.php');
+}
+
+$nivel = $_SESSION["nivel"];
+
+if ($nivel == 1) {
+    $tipo = "Administrador";
+    $menu = "fragments/menua.php";
+}
+
+if ($nivel == 2) {
+    $pk_sucursal = $_SESSION["pk_sucursal"];
+    $tipo = "Chofer";
+    $menu = "fragments/menub.php";
+}
+
+
+if ($nivel != 1) {
+    header('Location: ../index.php');
+}
+
+mysqli_set_charset($mysqli, 'utf8');
+?>
+
+
+<!doctype html>
+
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Posmovil - Consola de administración</title>
+    <link rel="stylesheet" href="vendors/feather/feather.css">
+    <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="vendors/typicons/typicons.css">
+    <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+
+    <link rel="stylesheet" href="js/select.dataTables.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="vendors/select2/select2.min.css">
+    <link rel="stylesheet" href="vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
+
+    <link rel="stylesheet" href="css/vertical-layout-light/style.css">
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <link rel="shortcut icon" href="images/user-sbg.png" />
+
+</head>
+
+<body>
+
+    <?php include $menu ?>
+
+    <!-- partial -->
+    <div class="main-panel">
+
+        <div class="row justify-content-center">
+            <div class="col-lg-10 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Entradas desde compras</h4>
+
+                        <div class="table-responsive overflow-hidden" id="tabla">
+                            <table id='dtEmpresa' class='table table-striped'>
+                                <thead>
+                                    <tr>
+                                        <th>#Entrada</th>
+                                        <th>Fecha</th>
+                                        <th>Sucursal/Almacén</th>
+                                        <th>Usuario</th>
+                                        <th>Observaciones</th>
+                                        <th>Ver</th>
+                                    </tr>
+                                </thead>
+                                <tfoot style='display: table-header-group'>
+                                    <tr>
+                                        <th>#Entrada</th>
+                                        <th>Fecha</th>
+                                        <th>Sucursal/Almacén</th>
+                                        <th>Usuario</th>
+                                        <th>Observaciones</th>
+                                        <th></th>
+                                    </tr>
+                                </tfoot>
+                                <tbody>
+                                    <?php
+
+                                    $nivel == 1 ? $flsucursal = "" : $flsucursal = " AND tr_entradas.fk_sucursal = $pk_sucursal";
+
+                                    $eusuario = "SELECT tr_entradas.*,
+                                            ct_sucursales.nombre as sucursal,
+                                            rt_sucursales_almacenes.nombre as almacen
+                                        FROM tr_entradas, ct_sucursales, rt_sucursales_almacenes
+                                        WHERE tr_entradas.estado = 1
+                                        AND tr_entradas.fk_compra > 0$flsucursal
+                                        AND ct_sucursales.pk_sucursal = tr_entradas.fk_sucursal
+                                        AND rt_sucursales_almacenes.pk_sucursal_almacen = tr_entradas.fk_almacen";
+
+                                    if (!$resultado = $mysqli->query($eusuario)) {
+                                        echo "Lo sentimos, esta aplicación está experimentando problemas.";
+                                        exit;
+                                    }
+
+                                    while ($row = $resultado->fetch_assoc()) {
+
+                                        echo <<<HTML
+                                            <tr class="odd gradeX">
+                                                <td>$row[pk_entrada]</td>
+                                                <td><i class='bx bx-calendar fs-5'></i> $row[fecha] $row[hora]</td>
+                                                <td>$row[sucursal] / $row[almacen]</td>
+                                                <td><i class='bx bx-user-circle fs-5'></i> $row[fk_usuario]</td>
+                                                <td style='white-space: normal;'>$row[observaciones]</td>
+                                                <td><a target='_blank' href='entradaPDF.php?id=$row[pk_entrada]'><i class='fa fa-file-pdf-o vpdf fa-lg btn-pdf'></i></a></td>
+                                            </tr>
+                                        HTML;
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                            <div class='row'>&nbsp;</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="assets/vendor/jquery-2.1.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="assets/plugins.js"></script>
+    <script src="assets/main.js"></script>
+    <script src="assets/lib/data-table/datatables.min.js"></script>
+    <script src="assets/lib/data-table/dataTables.bootstrap.min.js"></script>
+    <script src="assets/lib/data-table/dataTables.buttons.min.js"></script>
+    <script src="assets/lib/data-table/buttons.bootstrap.min.js"></script>
+    <script src="assets/lib/data-table/pdfmake.min.js"></script>
+    <script src="assets/lib/data-table/vfs_fonts.js"></script>
+    <script src="assets/lib/data-table/buttons.html5.min.js"></script>
+    <script src="assets/lib/data-table/buttons.print.min.js"></script>
+    <script src="assets/lib/data-table/buttons.colVis.min.js"></script>
+    <script src="assets/lib/data-table/datatables-init.js"></script>
+
+    <script>
+        //INPUTS DE FILTRADO POR COLUMNA
+        $('#dtEmpresa tfoot th').each(function() {
+            var title = $(this).text().trim();
+            if (title != '') {
+                $(this).html('<input type="text" class="form-control" placeholder="Buscar ' + title + '" />');
+            }
+        });
+
+        $('#dtEmpresa').DataTable({
+            initComplete: function() {
+                // Aplicar la búsqueda
+                this.api()
+                    .columns()
+                    .every(function() {
+                        var that = this;
+
+                        $('input', this.footer()).on('keyup change clear', function() {
+                            if (that.search() !== this.value) {
+                                that.search(this.value).draw();
+                            }
+                        });
+                    });
+            },
+
+            responsive: true,
+            ordering: true,
+            pageLength: 10,
+            order: [
+                [0, 'desc']
+            ],
+            dom: '<"dtEmpresa_header"lfp><t><rip>',
+            //lfptrip
+            language: {
+                "lengthMenu": "Mostrando _MENU_ registros por pagina",
+                "search": "Buscar:",
+                "zeroRecords": "No hay registros",
+                "info": "Mostrando pagina _PAGE_ de _PAGES_",
+                "infoEmpty": "Sin registros disponibles",
+                "infoFiltered": "(filtrando de _MAX_ registros totales)",
+                "paginate": {
+                    "previous": "Anterior",
+                    "next": "Siguiente"
+                }
+            }
+
+        });
+    </script>
+
+</body>
+
+</html>
